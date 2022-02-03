35e58a1a2b5e995856c43170b2b05dd91032a208
Kukla-Igor <31135224+Kukla-Igor@users.noreply.github.com>
2022-02-03 19:52:00 +0200

    Add files via upload

---
+++ SecTest/java/ua/kiev/prog/MyController.java
@@ -0,0 +1,144 @@
+package ua.kiev.prog;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+public class MyController {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public MyController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        User user = getCurrentUser();
+
+        String login = user.getUsername();
+        CustomUser dbUser = userService.findByLogin(login);
+
+        model.addAttribute("login", login);
+        model.addAttribute("roles", user.getAuthorities());
+        model.addAttribute("admin", isAdmin(user));
+        model.addAttribute("email", dbUser.getEmail());
+        model.addAttribute("phone", dbUser.getPhone());
+        model.addAttribute("address", dbUser.getAddress());
+        model.addAttribute("photo", dbUser.getPhotoPath());
+
+
+        return "index";
+    }
+
+    @PostMapping(value = "/update")
+    public String update(@RequestParam(required = false) String email,
+                         @RequestParam(required = false) String phone) {
+        User user = getCurrentUser();
+
+        String login = user.getUsername();
+        userService.updateUser(login, email, phone);
+
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/newuser")
+    public String update(@RequestParam String login,
+                         @RequestParam String password,
+                         @RequestParam(required = false) String email,
+                         @RequestParam(required = false) String phone,
+                         @RequestParam(required = false) String address,
+                         @RequestParam(required = false) MultipartFile photo,
+                         Model model) {
+        String passHash = passwordEncoder.encode(password);
+
+
+        String photoPath = null;
+        if (!photo.isEmpty()) {
+            try {
+                StringBuilder stringBuilder = new StringBuilder(photo.getContentType());
+                byte[] bytes = photo.getBytes();
+                Path path = Paths.get("C:Users\\Samog\\Desktop\\SavePhotoTest\\" + login + "." + stringBuilder.substring(stringBuilder.indexOf("/") + 1));
+                Files.write(path, bytes);
+                photoPath = path.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if ( ! userService.addUser(login, passHash, UserRole.USER, email, phone, address, photoPath)) {
+            model.addAttribute("exists", true);
+            model.addAttribute("login", login);
+            return "register";
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/delete")
+    public String delete(@RequestParam(name = "toDelete[]", required = false) List<Long> ids,
+                         Model model) {
+        userService.deleteUsers(ids);
+        model.addAttribute("users", userService.getAllUsers());
+
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ROLE_ADMIN')") // !!!
+    public String admin(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "admin";
+    }
+
+    @GetMapping("/unauthorized")
+    public String unauthorized(Model model){
+        User user = getCurrentUser();
+        model.addAttribute("login", user.getUsername());
+        return "unauthorized";
+    }
+
+    // ----
+
+    private User getCurrentUser() {
+        return (User)SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+    }
+
+    private boolean isAdmin(User user) {
+        Collection<GrantedAuthority> roles = user.getAuthorities();
+
+        for (GrantedAuthority auth : roles) {
+            if ("ROLE_ADMIN".equals(auth.getAuthority()))
+                return true;
+        }
+
+        return false;
+    }
+}
